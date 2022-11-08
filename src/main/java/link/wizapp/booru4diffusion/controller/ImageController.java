1f3d867aed451880aea27ad2a5b584e1a58ab029
Ballista01 <ballista01@outlook.com>
2022-11-08 15:20:38 -0500

    add getImageById REST API

--- src/main/java/link/wizapp/booru4diffusion/controller/ImageController.java
+++ src/main/java/link/wizapp/booru4diffusion/controller/ImageController.java
@@ -8,23 +8,20 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/images")
 public class ImageController {
 
     @Autowired
     IImageTgw imageTgw;
     private static final Logger log = LoggerFactory.getLogger(ImageController.class);
 
-    @GetMapping("/images")
+    @GetMapping("")
     public ResponseEntity<List<Image>> getAllImages(@RequestParam(required = false) String title) {
         try {
             List<Image> images = new ArrayList<Image>();
@@ -45,4 +42,19 @@ public class ImageController {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Image> getImageById(@PathVariable("id") long id) {
+        try {
+            Image image = imageTgw.findById(id);
+            if(image == null){
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }else{
+                return new ResponseEntity<>(image, HttpStatus.OK);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
