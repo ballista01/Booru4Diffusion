5c5cdc7e1736885802e57af33b4119a9d97873cf
Ballista01 <ballista01@outlook.com>
2022-11-07 19:10:31 -0500

    init

---
+++ src/main/java/link/wizapp/booru4diffusion/Booru4DiffusionApplication.java
@@ -0,0 +1,13 @@
+package link.wizapp.booru4diffusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Booru4DiffusionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Booru4DiffusionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/link/wizapp/booru4diffusion/Booru4DiffusionApplicationTests.java
@@ -0,0 +1,13 @@
+package link.wizapp.booru4diffusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Booru4DiffusionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
