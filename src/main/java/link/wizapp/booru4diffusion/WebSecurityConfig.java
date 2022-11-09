4123807b0974b642de1e187175acef1f60f8df12
Ballista01 <ballista01@outlook.com>
2022-11-08 19:51:50 -0500

    remove default security login

---
+++ src/main/java/link/wizapp/booru4diffusion/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package link.wizapp.booru4diffusion;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/api/**").permitAll()
+                        .anyRequest().permitAll()
+                );
+        return http.build();
+    }
+}
