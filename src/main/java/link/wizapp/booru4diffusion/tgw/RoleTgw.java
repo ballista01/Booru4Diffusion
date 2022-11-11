c7bea2077ee91168170053616065f1a50eaaeb90
Ballista01 <ballista01@outlook.com>
2022-11-10 20:55:05 -0500

    return generated id after update user and role

--- src/main/java/link/wizapp/booru4diffusion/tgw/RoleTgw.java
+++ src/main/java/link/wizapp/booru4diffusion/tgw/RoleTgw.java
@@ -6,7 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.Objects;
 
 @Repository
 public class RoleTgw implements IRoleTgw{
@@ -22,6 +27,30 @@ public class RoleTgw implements IRoleTgw{
             return role;
         } catch (IncorrectResultSizeDataAccessException e) {
             return null;
+        }
+    }
+    public int save(Role role) {
+        int res = 0;
+        try {
+            Role existingRole = findByName(role.getName());
+            if(existingRole!=null){
+                return 0;
+            }
+
+            String insertQuery = "INSERT INTO roles (name) VALUES (?) RETURNING id";
+            GeneratedKeyHolder generatedKeyHolder = new GeneratedKeyHolder();
+            res += jdbcTemplate.update(conn -> {
+                PreparedStatement preparedStatement = conn.prepareStatement(insertQuery, Statement.RETURN_GENERATED_KEYS);
+                preparedStatement.setString(1, role.getName().toString());
+                return preparedStatement;
+            }, generatedKeyHolder);
+            Integer newId = Objects.requireNonNull(generatedKeyHolder.getKey()).intValue();
+            role.setId(newId);
+
+            res += jdbcTemplate.update("INSERT INTO roles (name) VALUES (?)", role.getName());
+            return res;
+        } catch (IncorrectResultSizeDataAccessException e) {
+            return -1;
         }
     }
 }
--- src/main/java/link/wizapp/booru4diffusion/tgw/UserTgw.java
+++ src/main/java/link/wizapp/booru4diffusion/tgw/UserTgw.java
@@ -1,12 +1,18 @@
 package link.wizapp.booru4diffusion.tgw;
 
+import link.wizapp.booru4diffusion.model.Role;
 import link.wizapp.booru4diffusion.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.Objects;
 import java.util.Optional;
 
 @Repository
@@ -14,6 +20,9 @@ public class UserTgw implements IUserTgw{
 
     @Autowired
     JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    RoleTgw roleTgw;
 
     @Override
     public User findByUsername(String username) {
@@ -41,25 +50,59 @@ GROUP BY users.id;
 
     @Override
     public Boolean existsByEmail(String email) {
-        if(findByEmail(email) != null){
-            return true;
-        } else {
-            return false;
-        }
+        return findByEmail(email) != null;
     }
 
     public User findByEmail(String email) {
         try {
-            User user = jdbcTemplate.queryForObject("SELECT * FROM users WHERE email=?",
-                    BeanPropertyRowMapper.newInstance(User.class), email);
+            User user = jdbcTemplate.queryForObject(
+                    """
+SELECT users.id, username, email, password, ARRAY_AGG(roles.name) AS roles_name, ARRAY_AGG(roles.id) AS roles_id
+FROM users
+LEFT JOIN user_roles ON users.id = user_roles.user_id
+LEFT JOIN roles ON user_roles.role_id = roles.id
+WHERE email=?
+GROUP BY users.id;
+""",
+                    new UserRowMapper(), email);
             return user;
-        } catch(IncorrectResultSizeDataAccessException e){
+        } catch (IncorrectResultSizeDataAccessException e){
             return null;
         }
     }
     @Override
     public int save(User user) {
-        return jdbcTemplate.update("INSERT INTO users (email, password, username) VALUES(?, ?, ?)",
-                new Object[]{ user.getEmail(), user.getPassword(), user.getUsername() });
+        int res = 0;
+        try{
+//            long newId = jdbcTemplate.update("INSERT INTO users (email, password, username) VALUES(?, ?, ?) RETURNING id",
+//                    new Object[]{ user.getEmail(), user.getPassword(), user.getUsername() });
+//            user.setId(newId);
+
+            String insertQuery = "INSERT INTO users (email, password, username) VALUES(?, ?, ?) RETURNING id";
+            GeneratedKeyHolder generatedKeyHolder = new GeneratedKeyHolder();
+            res += jdbcTemplate.update(conn -> {
+                PreparedStatement preparedStatement = conn.prepareStatement(insertQuery, Statement.RETURN_GENERATED_KEYS);
+                preparedStatement.setString(1, user.getEmail());
+                preparedStatement.setString(2, user.getPassword());
+                preparedStatement.setString(3, user.getUsername());
+                return preparedStatement;
+            }, generatedKeyHolder);
+            long newId = Objects.requireNonNull(generatedKeyHolder.getKey()).longValue();
+            user.setId(newId);
+
+            res++;
+            for(Role role: user.getRoles()) {
+                // Create entries for roles which not yet exist in the roles table.
+                if(role.getId() == null){
+                    roleTgw.save(role);
+                }
+
+                res += jdbcTemplate.update("INSERT INTO user_roles (user_id, role_id) VALUES(?, ?)",
+                        new Object[] {user.getId(), role.getId()});
+            }
+            return res;
+        } catch (DataAccessException e) {
+            return -1;
+        }
     }
 }
